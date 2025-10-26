c06fc073619e33fc97112bf93245fbe8d47ed989
Smiteshbhore21 <smiteshbhore23@gmail.com>
2025-10-26 17:20:31 +0530

    Initial Render deployment

---
+++ backend_flask/main.py
@@ -0,0 +1,36 @@
+from flask import Flask, request
+from flask_cors import CORS
+import pandas as pd
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+CORS(app)
+
+model = pickle.load(open("LinearRegressionModel.pkl", "rb"))
+car = pd.read_csv("Cleaned_Car.csv")
+
+
+@app.route("/predict", methods=["GET"])
+def predictGet():
+    return "<h1> This is POST route </h1>"
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    company = request.form.get("company")
+    car_model = request.form.get("car_model")
+    year = int(request.form.get("year"))
+    fuel_type = request.form.get("fuel_type")
+    kms_driven = int(request.form.get("kms_driven"))
+    prediction = model.predict(
+        pd.DataFrame(
+            [[car_model, company, year, kms_driven, fuel_type]],
+            columns=["name", "company", "year", "kms_driven", "fuel_type"],
+        )
+    )
+    return str(np.round(prediction[0], 2))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
